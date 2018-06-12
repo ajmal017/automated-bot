80b01e43f7efff5b25926256e089c11b46ab3b9f
Uroš Jevremović <jevremovic.uros91@gmail.com>
2018-06-12 11:14:24 +0200

    fixed an edge case that was causing bot to enter into infinite loop

--- automated_bot.py
+++ automated_bot.py
@@ -14,13 +14,19 @@ list_of_valid_temporary_emails = ['zikoxih@getnada.com', 'wasyso@getnada.com', '
                                   'kuboluf@getnada.com', 'gysuqu@getnada.com', 'bysy@getnada.com',
                                   'cuwifuso@getnada.com', 'zedyby@getnada.com', 'ratanoro@getnada.com']
 
+
+def get_next_valid_mail():
+    yield list_of_valid_temporary_emails.pop()
+
+
 test_user_name = 'Test'
 generic_password = 'passmeup123'
 
 
 def populate_users_and_create_posts():
     for _ in range(0, rules['number_of_users']):
-        valid_email = list_of_valid_temporary_emails.pop()
+        valid_email = next(get_next_valid_mail())
+        print(valid_email)
         current_time = time.time()
         username = f'{test_user_name}-{current_time}'
         requests.post('http://127.0.0.1:8000/api/register/',
@@ -38,15 +44,15 @@ def populate_users_and_create_posts():
 def perform_like_activity():
     post_with_zero_likes_exists = True
     while post_with_zero_likes_exists:
-        max_number_of_posts = 0
+        current_max_number_of_posts = 0
         user = ''
         get_all_users = requests.get(f'http://127.0.0.1:8000/api/user-list/', headers={'Connection': 'close'})
         for each_user in get_all_users.json():
             number_of_posts = len(each_user['blog_posts'])
             number_of_likes = len(each_user['posts_liked'])
             if number_of_likes < rules['max_likes_per_user']:
-                if number_of_posts > max_number_of_posts:
-                    max_number_of_posts = number_of_posts
+                if number_of_posts > current_max_number_of_posts:
+                    current_max_number_of_posts = number_of_posts
                     user = [each_user['username'], generic_password, number_of_likes, each_user['id']]
 
         if user == '':
@@ -64,12 +70,13 @@ def perform_like_activity():
                 list_of_posts_from_other_users.append(post)
         get_all_users = requests.get(f'http://127.0.0.1:8000/api/user-list/', headers={'Connection': 'close'})
         if not list_of_posts_from_other_users:
+            current_max_number_of_posts = 0
             for each_user in get_all_users.json():
                 number_of_posts = len(each_user['blog_posts'])
                 number_of_likes = len(each_user['posts_liked'])
                 if number_of_likes < rules['max_likes_per_user'] and each_user['username'] != user[0]:
-                    if number_of_posts > max_number_of_posts:
-                        max_number_of_posts = number_of_posts
+                    if number_of_posts > current_max_number_of_posts:
+                        current_max_number_of_posts = number_of_posts
                         user = [each_user['username'], generic_password, number_of_likes, each_user['id']]
                         get_token_for_user_with_max_number_of_posts = requests.post('http://127.0.0.1:8000/api/token/',
                                                                                     data={'username': user[0],
@@ -78,6 +85,8 @@ def perform_like_activity():
                         token = get_token_for_user_with_max_number_of_posts.json()['access']
                         headers = {'Authorization': f'Bearer {token}', 'Connection': 'close'}
 
+        list_of_posts_with_no_likes_from_other_users = [post for post in list_of_posts_from_other_users if
+                                                        post['users_like'] == 0]
         for each_post in list_of_posts_from_other_users:
             if len(each_post['users_like']) == 0:
                 post_pk_of_random_post_from_user_that_has_a_post_with_no_likes = random.choice(
@@ -85,13 +94,11 @@ def perform_like_activity():
                 post_to_like = requests.get(f"http://127.0.0.1:8000/api/post-list/"
                                             f"{random.choice(each_post['author']['blog_posts'])}",
                                             headers={'Connection': 'close'})
-                print(post_to_like)
                 while True:
                     if user[3] not in post_to_like.json()['users_like']:
-                        like_post_with_no_likes = requests.get(
-                            f'http://127.0.0.1:8000/api/post-list/'
-                            f'{post_pk_of_random_post_from_user_that_has_a_post_with_no_likes}/like',
-                            headers=headers)
+                        requests.get(f'http://127.0.0.1:8000/api/post-list/'
+                                     f'{post_pk_of_random_post_from_user_that_has_a_post_with_no_likes}/like',
+                                     headers=headers)
                         user[2] += 1
                         break
                     post_pk_of_random_post_from_user_that_has_a_post_with_no_likes = random.choice(
@@ -99,7 +106,7 @@ def perform_like_activity():
                     post_to_like = requests.get(f'http://127.0.0.1:8000/api/post-list/'
                                                 f'{post_pk_of_random_post_from_user_that_has_a_post_with_no_likes}',
                                                 headers={'Connection': 'close'})
-            if user[2] == rules['max_likes_per_user']:
+            if user[2] == rules['max_likes_per_user'] or not list_of_posts_with_no_likes_from_other_users:
                 break
         for each_post in get_all_posts.json():
             if len(each_post['users_like']) == 0:
